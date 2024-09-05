52cb0cb86945843b2bcc93866ffa7e12d4b4f343
sasavas <alpsavash@gmail.com>
2024-09-05 17:37:30 +0300

    udpate config for deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/alpsavas',
+    assetPrefix: '/alpsavas/',
+    images: {
+        unoptimized: true, // GitHub Pages doesn't support Next.js optimized images
+    },
 };
 
 export default nextConfig;
